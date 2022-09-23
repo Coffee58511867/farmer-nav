e1b375c1d8a6eb0df4b9d1c53492629540804535
Coffee58511867 <marobahlalele910@gmail.com>
2022-09-23 10:46:52 +0200

    Added Skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,35 +1,206 @@
-import React from 'react'
-import { Container, Flex, Box , Text, Heading , SimpleGrid , Image , Stack} from '@chakra-ui/react'
-import java from '../images/java.png';
-import mo from '../images/mo.png';
-import php from '../images/php.png'
-import re from '../images/type.png'
-import postg from '../images/postg.png'
-import c from '../images/c.png'
-import NavBar from './Header'
-import Footer from './Footer'
+import React from "react";
+import {
+  Container,
+  Box,
+  Heading,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
+import NavBar from "./Header";
+import Footer from "./Footer";
+import {
+  FaJava,
+  FaDatabase,
+  FaPhp,
+  FaReact,
+  FaAndroid,
+  FaWindows,
+  FaLinux,
+  FaLaptop,
+  FaNetworkWired,
+  FaGithub,
+  FaNodeJs,
+} from "react-icons/fa";
 
 const Skills = () => {
   return (
     <div>
-      <NavBar/>
-      <Container  maxW={'5xl'} py={12}>
+      <NavBar />
+      <Container maxW={"5xl"} py={12}>
         <Stack p={10}>
-        <Heading size={["1xl" , "1x1" , "2xl"]} color="blue.900">My Skills</Heading>
-      <SimpleGrid minChildWidth='120px' spacing='40px'>
-  <Box bg='tomato' height='80px'><Image src={java} /></Box>
-  <Box bg='tomato' height='80px'><Image src={mo} /></Box>
-  <Box bg='tomato' height='80px'><Image src={php} /></Box>
-  <Box bg='tomato' height='80px'><Image src={re} /></Box>
-  <Box bg='tomato' height='80px'><Image src={postg} /></Box>
-  <Box bg='tomato' height='80px'><Image src={c} /></Box>
-  
-</SimpleGrid>
-</Stack>
-      </Container>
-      <Footer/>
-    </div>
-  )
-}
+          <SimpleGrid minChildWidth="120px" spacing="40px">
+            <Box>
+              <FaJava fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Desktop Applications
+              </Heading>
+            </Box>
+            <Box>
+              <FaReact fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                React JavaScript
+              </Heading>
+            </Box>
+            <Box>
+              <FaDatabase fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Mongodb
+              </Heading>
+            </Box>
+            <Box>
+              <FaPhp fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Php
+              </Heading>
+            </Box>
+            <Box>
+              <FaJava fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                TypeScript
+              </Heading>
+            </Box>
+            <Box>
+              <FaDatabase fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                PostgreSQL
+              </Heading>
+            </Box>
+            <Box>
+              <FaDatabase fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Mysql
+              </Heading>
+            </Box>
 
-export default Skills
+            <Box>
+              <FaJava fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                C++
+              </Heading>
+            </Box>
+            <Box>
+              <FaAndroid fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Mobile App Development
+              </Heading>
+            </Box>
+            <Box>
+              <FaWindows fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Windows Operating Systems
+              </Heading>
+            </Box>
+            <Box>
+              <FaLinux fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Linux Operating Systems
+              </Heading>
+            </Box>
+            <Box>
+              <FaLaptop fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Computer Repair
+              </Heading>
+            </Box>
+            <Box>
+              <FaNetworkWired fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Network
+              </Heading>
+            </Box>
+            <Box>
+              <FaDatabase fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Data Science
+              </Heading>
+            </Box>
+          </SimpleGrid>
+        </Stack>
+        <Stack p={10}>
+          <Heading mt={10} mb={12}>Technology</Heading>
+          <SimpleGrid minChildWidth="120px" spacing="40px">
+            <Box>
+              <FaGithub fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Github
+              </Heading>
+            </Box>
+            <Box>
+              <FaNodeJs fontSize="50px" />
+              <Heading
+                size={["sm", "sm", "sm"]}
+                color="blue.900"
+                fontWeight={800}
+              >
+                Node js
+              </Heading>
+            </Box>
+          </SimpleGrid>
+        </Stack>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default Skills;
